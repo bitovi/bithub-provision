61c7b94363c1d97002189f73cd74526ea01fab6c
Nikica Jokic <neektza@gmail.com>
2013-07-24 11:45:31 +0200

    Added some dependencies.

--- cookbook/metadata.rb
+++ cookbook/metadata.rb
@@ -6,10 +6,10 @@ description      'Installs/Configures bithub'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends "apt", "<= 1.10.0"
 depends "database"
 depends "postgresql"
 depends "nginx"
 depends "rabbitmq"
 depends "rbenv"
+depends "imagemagick"
 depends "vim"
