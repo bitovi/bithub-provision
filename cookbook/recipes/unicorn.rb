fd765a3bacd5753939aec37bb44345e74d4e9682
Nikica Jokic <neektza@gmail.com>
2013-07-24 18:04:41 +0200

    Create a unicorn logfile.
    Since only root can tamper with /var/log.

--- cookbook/recipes/unicorn.rb
+++ cookbook/recipes/unicorn.rb
@@ -2,3 +2,10 @@ template node[:bithub][:backend][:config] do
   source 'unicorn.rb.erb'
   mode '755'
 end
+
+file "/var/log/unicorn.log" do
+  owner "vagrant"
+  group "vagrant"
+  mode "0666"
+  action :create
+end
