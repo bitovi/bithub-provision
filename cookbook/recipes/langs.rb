d59c9ff3ee69f4a3f1429c9b8c1f20bf9cd5521b
Nikica Jokic <neektza@gmail.com>
2013-08-27 16:08:15 +0200

    Wrong reference to prop in settings hash.

--- cookbook/recipes/langs.rb
+++ cookbook/recipes/langs.rb
@@ -1,7 +1,7 @@
 include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
 
-rbenv_ruby node[:bithub][:pls][:ruby_version]
+rbenv_ruby node[:bithub][:langs][:ruby_version]
 rbenv_gem "bundler" do
   ruby_version node[:bithub][:langs][:ruby_version] 
 end
