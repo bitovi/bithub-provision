76cfebd2b8c4ee310fafc2e12147be1f29e47188
Nikica Jokic <neektza@gmail.com>
2013-11-08 16:06:22 +0100

    Cookbook for rabbitmq set to version > 2.3

--- cookbook/metadata.rb
+++ cookbook/metadata.rb
@@ -4,13 +4,13 @@ maintainer_email 'neektza@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures Bithub'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
-depends "rbenv"
-depends "imagemagick"
-depends "database"
-depends "postgresql"
-depends "nginx"
-depends "rabbitmq"
-depends "vim"
-depends "tmux"
+depends 'rbenv'
+depends 'imagemagick'
+depends 'database'
+depends 'postgresql'
+depends 'nginx'
+depends 'rabbitmq', '> 2.3'
+depends 'vim'
+depends 'tmux'
